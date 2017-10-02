1ef4690e5789cfab55b81c1cff2f48205f663e52
yone <msn_07zgok@yahoo.co.jp>
2017-10-02 04:46:06 +0000

    17 で割り切れるものだけにする seventeen モジュール

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
+const a = [];
 
+function isMultipleOfSeventeen(n) {
+    return n % 17 === 0;
+}
+/*要らない
+function seventeen(a) {
+    return Array.from.firter(isMultipleOfSeventeen)
+}
+*/
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
